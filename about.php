98f215573757923e55908e30d5dd223921b451e4
kurt <kurta.tomas@gmail.com>
2021-02-03 17:13:09 +0100

    public

--- about.php
+++ about.php
@@ -13,64 +13,7 @@
 
 
                 <div class="container">
-                    <h3>Osobné údaje:</h3>
-                    <p>
-                        narodený 7. 5. 1991 v Bratislave<br>
-                        slovenská národnosť</p><br><br>
-                    <h3>Vzdelanie:</h3>
-                    <p>
-                        - 2006 – 2010, Stredná priemyselná škola elektrotechnická
-                        K. Adlera 5, 841 02  Bratislava– maturita<br>
-                        odbor: počítačové systémy<br>
-                        - 2012 - 2014, Fakulta elektrotechniky a informatiky
-                        STU v Bratislave<br>
-                        odbor: automobilová elektronika
 
-                    </p><br><br>
-                </div>
-
-                <div class="container">
-                    <h3>Zamestnanie:</h3>
-                    <p>
-                        - 2010 – 2012
-                        PPI – ETC Slovakia s. r. o.,<br>
-                        servisný technik mobilných telefónov<br>
-                        - 2014 – 2017
-                        Dokument Logistik s. r. o.,<br>
-                        vodič<br>
-                        - 2017 – 2018
-                        Lion´s Horeca Industry s.r.o.,<br>
-                        servisný technik<br>
-                        - 2018 -2020
-                        Todos Bratislava s.r.o.,<br>
-                        autoelektrikár diagnostik<br>
-                        - 2020-2021
-                        Final-CD s.r.o. Bratislava,<br>
-                        autoelektrikár diagnostik<br>
-
-                    </p><br><br>
-                </div>
-
-                <div class="container">
-                    <h3>Jazykové znalosti:</h3>
-                    <p>
-                        anglický jazyk – slovom a písmom</p><br><br>
-
-                    <h3>Odborné znalosti:</h3>
-                    <p>
-                        -Samostatný elektrotechnik v zmysle § 22<br>
-                        -absolvované kurzy HTML PHP MySQL</p><br><br>
-
-
-
-
-
-
-                    <p>Bratislava,  01.02.2021<br><br><br><br>
-
-                        Tomáš Kurta
-
-                    </p>
 
                 </div>
 
